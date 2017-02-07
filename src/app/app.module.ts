bcb86f2c4339e2d5a3701b3d32c8d17983611889
raavr <rafal.r26@gmail.com>
2017-02-08 00:50:53 +0100

    removed unnecessary comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
-/*
- * Platform and Environment providers/directives/pipes
- */
+
 import { ENV_PROVIDERS } from './environment';
 import { ROUTES } from './app.routes';
-// App is our top level component
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { NavbarComponent } from './navbar';
@@ -27,15 +24,11 @@ const APP_SERVICE_PROVIDERS = [
 
 ];
 
-// Application wide providers
 const APP_PROVIDERS = [
   ...APP_RESOLVER_PROVIDERS,
   ...APP_SERVICE_PROVIDERS
 ];
 
-/**
- * `AppModule` is the main entry point into Angular2's bootstraping process
- */
 @NgModule({
   bootstrap: [ AppComponent ],
   declarations: [
@@ -45,7 +38,7 @@ const APP_PROVIDERS = [
     NoContentComponent,
     CollapseNavbarDirective
   ],
-  imports: [ // import Angular's modules
+  imports: [ 
     BrowserModule,
     FormsModule,
     HttpModule,
@@ -57,7 +50,7 @@ const APP_PROVIDERS = [
     AgendaModule,
     RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules }),
   ],
-  providers: [ // expose our Services and Providers into Angular's dependency injection
+  providers: [
     ENV_PROVIDERS,
     APP_PROVIDERS
   ]
